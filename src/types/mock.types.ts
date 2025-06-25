997573fcfde73e63b8b3fe7f56178313d8693533
Marc Font Puig <mfontpui@emeal.nttdata.com>
2025-06-25 18:01:07 +0200

    wip: mcokv3

--- src/types/mock.types.ts
+++ src/types/mock.types.ts
@@ -2,7 +2,7 @@ import { OpenAPI } from "./openapi.types";
 
 export type BaseMock = {
   getObjectFromRef: <T>(ref: string[]) => T;
-  getOutputSchema: (schema: OpenAPI.SchemaObject | OpenAPI.ReferenceObject, mockRefs: string[]) => Record<string, unknown> | unknown[];
+  getOutputSchema: <T extends OpenAPI.SchemaObject>(schema: T, mockRefs: string[]) => Record<string, unknown> | unknown[];
 };
 
 export type BaseMockStatic<T extends OpenAPI.Document> = {
